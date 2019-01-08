110083e33e556a814ba15a15a79c708b89ff6152
Chris Wendt <chrismwendt@gmail.com>
2019-01-08 15:23:11 -0800

    fix string passing

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 build:
 	emcc a.cc \
-	  -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "addFunction"]' \
+	  -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "addFunction", "Pointer_stringify"]' \
 	  -s EXPORTED_FUNCTIONS='["_newLSPLoop","_LSPLoop_send"]' \
 	  -s RESERVED_FUNCTION_POINTERS=1
 
--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var Module = require('./a.out.js')
 
 Module['onRuntimeInitialized'] = function() {
   const onResponse = Module.addFunction(function(response) {
-    console.log('response (this should print \'hey\'):', response)
-  }, ['string'])
+    console.log('response (this should print \'hey\'):', Module.Pointer_stringify(response))
+  }, 'vi')
   const lspLoop = Module.ccall('newLSPLoop', 'number', ['number'], [onResponse])
   Module.ccall('LSPLoop_send', null, ['number', 'string'], [lspLoop, 'hey'])
 }
